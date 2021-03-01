6c8639416cc1fbfa67c261c3a824a79783106909
Zoey Farago <74753125+ZoeyF75@users.noreply.github.com>
2021-03-01 07:28:30 -0800

    Add files via upload
    initial commit game outline notes

---
+++ mathGameRuby.rb/game.rb
@@ -0,0 +1,21 @@
+class Player
+  def initialize(isP1)
+    puts "Welcome"
+    lives = 3
+  end
+  #start game loop here
+  #isP1 ? create P1 : create P2
+  #set current player here
+end
+
+class P1 < Player
+  #keeps track of lives, if wrong then -- if 0 then game over
+end
+
+class P2 < Player
+  #keeps track of lives
+end
+
+p1 = Player.new
+
+
